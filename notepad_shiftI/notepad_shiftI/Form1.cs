007496a33e93840c3562920d650905fb025ae1be
jbzbernard <70106583+jbzbernard@users.noreply.github.com>
2026-07-19 19:14:16 +0530

    Add files via upload

---
+++ notepad_shiftI/notepad_shiftI/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace notepad_shiftI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void customizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            richTextBox1.Font = fontDialog1.Font;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           // openFileDialog1.Filter = "All Files(*)|*.*|Text files(.txt)|*.txt";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                this.Text = openFileDialog1.SafeFileName;
+            }
+
+        }
+
+        private void backgroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            richTextBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void foregroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            richTextBox1.ForeColor = colorDialog1.Color;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "untitled";
+
+        }
+    }
+}
